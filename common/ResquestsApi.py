5c013ee435bbd0cf26f1b01ff07bc85380b52128
HPJ <1491769514@qq.com>
2023-05-07 15:47:28 +0800

    初始化仓库

---
+++ common/ResquestsApi.py
@@ -0,0 +1,8 @@
+import requests
+
+def send_request(method,url,headers=None,**kwargs):
+    if method.lower() == "get":
+        pass
+    elif method.lower() == "post":
+        pass
+    elif method.lower()
